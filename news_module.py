4399a12373a0973a2e6486f1f99a913291feb9da
Naillin <kamilasd@yandex.ru>
2024-08-10 00:41:59 +0500

    modifed searh logic for news module. add comand support

--- news_module.py
+++ news_module.py
@@ -1,66 +1,85 @@
 import requests
 import random
+import datetime
 from news_unit import NewsUnit
 from newsapi import NewsApiClient
 
-# Получение war новостей
-def fetch_war_news(NEWS_API_KEY):
-    url = f'https://newsapi.org/v2/top-headlines?country=ru&category=war&language=ru&apiKey={NEWS_API_KEY}'
-    response = requests.get(url)
-    data = response.json()
-    articles = data.get('articles', [])
-    if articles:
-        random_article = random.choice(articles)
-        return random_article['title']
-    return 'Нет доступных новостей.'
+categories = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'science',
+    'sports',
+    'technology'
+]
 
-# Получение gaming новостей
-def fetch_gaming_news(NEWS_API_KEY):
-    url = f'https://newsapi.org/v2/top-headlines?country=ru&category=gaming&language=ru&apiKey={NEWS_API_KEY}'
-    response = requests.get(url)
-    data = response.json()
-    articles = data.get('articles', [])
-    if articles:
-        random_article = random.choice(articles)
-        return random_article['title']
-    return 'Нет доступных новостей.'
+global articles_date
+def fetch_newsAPI(NEWS_API_KEY):
+    """
+    Fetches news articles from the News API using the provided API key.
 
-global top_headlines
-def fetch_war_newsAPI(NEWS_API_KEY):
+    Args:
+        NEWS_API_KEY (str): The API key for the News API.
+
+    Returns:
+        None
+
+    Notes:
+        This function uses the NewsApiClient to fetch news articles.
+        It first tries to fetch top headlines, and if that fails, it tries to fetch everything.
+        The fetched articles are stored in the global variable articles_date.
+    """
+    # Открываем файл для чтения
+    with open('codewords.txt', 'r', encoding='utf-8') as file:
+        codewords = []
+        for line in file:
+            if line:  # Проверяем, что строка не пустая
+                codewords.append(line.strip())  # Сохраняем в список, убирая лишние пробелы
+
     # Init
     newsapi = NewsApiClient(api_key=NEWS_API_KEY)
-    # /v2/top-headlines
-    top_headlines = newsapi.get_top_headlines(category='war',
-                                              language='ru',
-                                              country='ru')
+
+    global articles_date  # Объявляем articles_date как глобальную переменную
+    articles_date = {'articles': []}  # Инициализируем переменную articles_date
+    while not articles_date['articles']:
+        d = random.choice(codewords)
+        s = random.choice(categories)
+        # /v2/top-headlines
+        articles_date = newsapi.get_top_headlines(
+            q=d,
+            category=s,
+            language='ru',
+            # country='ru'
+        )
+
+        if not articles_date['articles']:
+            # Если новостей не найдено, ищем их с помощью get_everything
+            now = datetime.datetime.now()
+            past_7_days = now - datetime.timedelta(days=7)
+            # /v2/everything
+            everything = newsapi.get_everything(
+                q=d,
+                from_param=past_7_days.strftime('%Y-%m-%d'),
+                to=now.strftime('%Y-%m-%d'),
+                language='ru',
+                # country='ru'
+            )
+            articles_date = everything
+
     # /v2/top-headlines/sources
     sources = newsapi.get_sources()
 
-def fetch_gaming_newsAPI(NEWS_API_KEY):
-    # Init
-    newsapi = NewsApiClient(api_key=NEWS_API_KEY)
-    # /v2/top-headlines
-    global top_headlines
-    top_headlines = newsapi.get_top_headlines(category='gaming',
-                                              language='ru',
-                                              country='ru')
-    # /v2/top-headlines/sources
-    sources = newsapi.get_sources()
-
-def fetch_technology_newsAPI(NEWS_API_KEY):
-    # Init
-    newsapi = NewsApiClient(api_key=NEWS_API_KEY)
-    # /v2/top-headlines
-    global top_headlines
-    top_headlines = newsapi.get_top_headlines(category='technology',
-                                              language='ru',
-                                              country='ru')
-    # /v2/top-headlines/sources
-    sources = newsapi.get_sources()
 
 def get_NewsUnit():
-    global top_headlines
-    articles = top_headlines['articles']
+    """
+    Retrieves a NewsUnit object containing information about a randomly selected news article.
+
+    Returns:
+        NewsUnit: A NewsUnit object containing information about a news article, or a string indicating that no news is available.
+    """
+    global articles_date
+    articles = articles_date['articles']
     if articles:
         random_article = random.choice(articles)
         return NewsUnit(
--- news_unit.py
+++ news_unit.py
@@ -1,5 +1,19 @@
+
 class NewsUnit:
     def __init__(self, author, title, description, url, urlToImage):
+        """
+        Initializes a NewsUnit object.
+
+        Args:
+            author (str): The author of the news article.
+            title (str): The title of the news article.
+            description (str): A brief description of the news article.
+            url (str): The URL of the news article.
+            urlToImage (str): The URL of an image associated with the news article.
+
+        Returns:
+            None
+        """
         self.author = author
         self.title = title
         self.description = description
@@ -7,4 +21,10 @@ class NewsUnit:
         self.urlToImage = urlToImage
 
     def __str__(self):
+        """
+        Returns a string representation of the NewsUnit object.
+
+        Returns:
+            str: The title of the news article.
+        """
         return self.title
