a1443f58506950ccef06e2efa88e7e853affb0f8
Naillin <kamilasd@yandex.ru>
2024-08-09 19:44:55 +0500

    add fun init_config for filling base fields for bot. all const values moved to config file next to main file.

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from discord.ext import tasks
 
 client = discord.Client()
 
+# Начальная настройка бота и запуск конфига
 def init_config():
     global DISCORD_TOKEN, CHANNEL_ID, NEWS_API_KEY, WEATHER_API_KEY, WEATHER_CITY
     # Открываем файл для чтения
@@ -24,6 +25,7 @@ def init_config():
     WEATHER_API_KEY = config['WEATHER_API_KEY']
     WEATHER_CITY = config['WEATHER_CITY']
 
+# Получение новостей
 def fetch_news():
     url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={NEWS_API_KEY}'
     response = requests.get(url)
@@ -33,6 +35,7 @@ def fetch_news():
         return articles[0]['title']  # Возвращаем заголовок первой статьи
     return 'Нет доступных новостей.'
 
+# Получение погоды
 def fetch_weather():
     url = f'https://api.openweathermap.org/data/2.5/weather?q={WEATHER_CITY}&appid={WEATHER_API_KEY}&units=metric'
     response = requests.get(url)
@@ -41,6 +44,7 @@ def fetch_weather():
     temperature = data['main']['temp']
     return f'Погода в {WEATHER_CITY}: {weather_description}, {temperature}°C'
 
+# Отправка новостей
 async def post_updates():
     channel = client.get_channel(CHANNEL_ID)
     if channel:
@@ -49,14 +53,17 @@ async def post_updates():
         await channel.send(f'Новости: {news}')
         await channel.send(f'Погода: {weather}')
 
+# Задача на отправку новостей (раз в час)
 @tasks.loop(hours=1)
 async def scheduled_task():
     await post_updates()
 
+# Заявление о том что бот в системе
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
     scheduled_task.start()
 
+# Запуск бота
 init_config()
 client.run(DISCORD_TOKEN)
