8c8b834cd90796b8a47dc6848b873bf61aef0d8d
OwlMan <vip.vlad.9999@mail.ru>
2024-08-10 00:45:56 +0500

    add weather module

--- weather_module.py
+++ weather_module.py
@@ -1,10 +1,30 @@
 import requests
+import discord
+
+from discord import Embed
 # Получение погоды
-def fetch_weather(WEATHER_API_KEY, WEATHER_CITY):
+async def weather(ctx, WEATHER_API_KEY, *, WEATHER_CITY: str):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={WEATHER_CITY}&appid={WEATHER_API_KEY}&units=metric'
     response = requests.get(url)
     data = response.json()
-    weather_description = data['weather'][0]['description']
-    temperature = data['main']['temp']
-    return f'Погода в {WEATHER_CITY}: {weather_description}, {temperature}°C'
+    channel = ctx.message.channel
+
+    y=data["main"]
+    current_temperature = y["Температура"]
+    current_temperature_celsiuis = str(round(current_temperature - 273.15))
+    current_pressure = y["Давление"]
+    current_humidity = y["Влажность"]
+    z=data["Погода"]
+
+    weather_description = z[0]["Описание"]
+    embed = discord.Embed(
+        title=f"Погода в городе {WEATHER_CITY}", color=ctx.guild.me.top_role.color, timestamp=ctx.message.created_at,)
+    embed.add_field(name="Описание", value=f"**{weather_description}**", inline=False)
+    embed.add_field(name="Температура(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
+    embed.add_field(name="Влажность(%)", value=f"**{current_humidity}%**", inline=False)
+    embed.add_field(name="Атмосферное давление(hPa)", value=f"**{current_pressure}hPa**", inline=False)
+    embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+    embed.set_footer(text=f"Requested by {ctx.author.name}")
+
+    await channel.send(embed=embed)
 
